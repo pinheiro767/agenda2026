35c95ae5dc09a4df76fb56eaa7bec282ca93c8cc
pinheiro767 <claudiapinheiro767@gmail.com>
2026-01-22 00:25:04 -0300

    Update filters.js

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -1,19 +1,6 @@
+// filters.js
+
 function getActiveProjectFilter() {
-  return document.getElementById("projectFilter").value;
-}
-
-function updateProjectFilter() {
   const select = document.getElementById("projectFilter");
-  const events = getEvents();
-
-  const projects = [...new Set(events.map(e => e.title))];
-
-  select.innerHTML = `<option value="all">Todos os projetos</option>`;
-  projects.forEach(p => {
-    select.innerHTML += `<option value="${p}">${p}</option>`;
-  });
+  return select ? select.value : "all";
 }
-
-document.getElementById("projectFilter").onchange = renderCalendar;
-
-updateProjectFilter();
