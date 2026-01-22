9156cf4596b2a694f2b299d42bd0fe7d27bcfa01
pinheiro767 <claudiapinheiro767@gmail.com>
2026-01-22 00:53:41 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,38 @@
+const CACHE_NAME = "agenda2026-cache-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./app.css",
+  "./calendar.js",
+  "./events.js",
+  "./filters.js",
+  "./storage.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("painel-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "assets/css/style.css",
-        "assets/js/app.js",
-        "manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
